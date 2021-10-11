0af79d8312db76c0e529ccc9d6fa172d5ac4175c
RONALD HERNANDEZ <ron@RONALDs-MacBook-Pro.local>
2021-10-11 12:18:00 -0600

    check if string contains unique chars

---
+++ Practice/InterviewPrep.playground/Pages/CheckIfStringContainsUniqueCharacters.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+/*
+ _   _      _                ___ _
+| | | |_ _ (_)__ _ _  _ ___ / __| |_  __ _ _ _ ___
+| |_| | ' \| / _` | || / -_) (__| ' \/ _` | '_(_-<
+ \___/|_||_|_\__, |\_,_\___|\___|_||_\__,_|_| /__/
+                |_|
+
+ Challenge: Given a string, see if you can detect whether it contains only unique chars.
+ 
+ */
+
+func isUnique(_ text: String) -> Bool {
+    guard !text.isEmpty else { return false }
+    
+    return text.count == Set(text).count
+}
+
+isUnique("ab")
+isUnique("aa")
+isUnique("abcdefghijklmnopqrstuvwxyz")
