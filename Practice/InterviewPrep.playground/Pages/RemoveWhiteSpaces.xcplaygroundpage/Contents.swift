da6b9ee3ea6d808000adddce9af7076f304744cb
RONALD HERNANDEZ <ron@RONALDs-MacBook-Pro.local>
2021-10-18 12:07:38 -0600

    remove white spaces

--- Practice/InterviewPrep.playground/Pages/RemoveWhiteSpaces.xcplaygroundpage/Contents.swift
+++ Practice/InterviewPrep.playground/Pages/RemoveWhiteSpaces.xcplaygroundpage/Contents.swift
@@ -1,7 +1,31 @@
-//: [Previous](@previous)
+/*
+ 
+ Write a function that returns a string with any consecutive spaces replaced with a single space.
+ Sample input and output
+ I’ve marked spaces using “[space]” below for visual purposes:
+ • The string “a[space][space][space]b[space][space][space]c” should return “a[space]b[space]c”.
+ • The string “[space][space][space][space]a” should return “[space]a”. • The string “abc” should return “abc”.
 
-import Foundation
+ */
 
-var greeting = "Hello, playground"
+func removeExtraWhiteSpaces(_ input: String) -> String {
+    guard input.count > 0 else { return input }
+    
+    var seenSpace = true
+    var resultString = ""
+    
+    for letter in input {
+        if letter == " " {
+            if seenSpace { continue }
+            seenSpace = true
+        } else {
+            seenSpace = false
+        }
+        
+        resultString.append(letter)
+    }
+    return resultString
+}
 
-//: [Next](@next)
+
+removeExtraWhiteSpaces("This     is       awesome!")
